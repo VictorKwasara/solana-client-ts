d19103976b5de578b5e86536d03f0bac83c984ca
root <root@DESKTOP-87DPNVK.localdomain>
2022-11-18 18:08:38 +0200

    pinging program complete

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,43 @@ import * as fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PROGRAM_ID = new Web3.PublicKey(
+	'ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa'
+);
+
+const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey(
+	'Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod'
+);
+
+
+async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair){
+    const transaction = new Web3.Transaction()
+    const instruction = new Web3.TransactionInstruction({
+        //Instruction need 3 things
+
+        //1. The public keys of all the accounts the instruction will read/write
+
+        keys: [
+            {
+                pubkey: PROGRAM_DATA_PUBLIC_KEY,
+                isSigner: false ,
+                isWritable: true
+            }
+        ],
+
+        // 2. THE  ID of the program this instruction will be sent to 
+        programId: PROGRAM_ID
+
+        //3. Data - in this case, there's none!
+    })
+
+    transaction.add(instruction)
+    const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+
+    console.log(
+        `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    )
+}
 
 async function initializeKeypair(connection: Web3.Connection): Promise<Web3.Keypair> {
     if (!process.env.PRIVATE_KEY) {
@@ -62,6 +99,7 @@ async function airdropSolIfNeeded(
 async function main() {
 const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
 const signer = await initializeKeypair(connection);
+await pingProgram(connection,signer)
 
 console.log("Public key:", signer.publicKey.toBase58());
    
